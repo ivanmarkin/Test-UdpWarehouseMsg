3f4a0711ef02098582108d32bdc818fb59c9b16d
ivan_markin <ivan.markin@sportsfusion.co.uk>
2026-01-21 04:16:55 +0300

    NATS publishing fix attempt - no luck

--- warehouse-service/src/main/java/net/ivm/lab/warehouse/out/NatsPublisher.java
+++ warehouse-service/src/main/java/net/ivm/lab/warehouse/out/NatsPublisher.java
@@ -3,8 +3,11 @@ package net.ivm.lab.warehouse.out;
 import io.nats.client.Connection;
 import io.nats.client.JetStream;
 import io.nats.client.Nats;
+import io.nats.client.Options;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 
 public class NatsPublisher {
     private static final Logger log = LoggerFactory.getLogger(NatsPublisher.class);
@@ -16,7 +19,14 @@ public class NatsPublisher {
 
     public NatsPublisher(String messageBrokerUrl, String messageTopic) {
         try {
-            this.connection = Nats.connect(messageBrokerUrl);
+            this.connection = Nats.connect(new Options.Builder()
+                    .server(messageBrokerUrl)
+                    .connectionTimeout(Duration.ofSeconds(10))
+                    .pingInterval(Duration.ofSeconds(2))
+                    .maxReconnects(-1)  // Infinite reconnects
+                    .reconnectWait(Duration.ofSeconds(2))
+                    .build()
+            );
             this.jetStream = connection.jetStream();
             this.messageTopic = messageTopic;
             log.info("Connected to msg broker at {}, messageTopic={}", messageBrokerUrl, messageTopic);
@@ -26,12 +36,13 @@ public class NatsPublisher {
     }
 
     public void publish(String message) {
-        try {
 //            String json = objectMapper.writeValueAsString(data);
 //            jetStream.publish(SensorData.MESSAGE_TOPIC, json.getBytes());
-            jetStream.publish(messageTopic, message.getBytes());
-        } catch (Exception e) {
-            log.error("Failed to publish: {}", e.getMessage());
-        }
+        jetStream.publishAsync(messageTopic, message.getBytes()).thenAccept(pubAck -> {
+            System.out.println("Published successfully: " + pubAck);
+        }).exceptionally(e -> {
+            log.error("Publish failed: {}", e.getMessage());
+            return null;
+        });
     }
 }
